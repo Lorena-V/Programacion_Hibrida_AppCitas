3841c42ee60bb2feeee4d685cae525a69c34bd9c
Lorena Gonzalez <gonzalezm.lorena2@gmail.com>
2025-01-02 09:02:12 -0300

    navegacion con icono config

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,23 +1,35 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';
+import { IonButton, IonIcon, IonButtons,
+          IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons'
+import { settingsOutline, buildOutline } from 'ionicons/icons'
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton],
+  standalone: true,
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon, IonButtons
+   ],
 })
+
 export class HomePage {
 
 
   constructor(private router:Router) {
+      addIcons({buildOutline});
 
   }
 
   irGestionOnClick(){
     console.log("Ir a gestion");
     this.router.navigate(['/gestion']);
+  }
+
+  irConfiguracionOnClick(){
+    console.log("Ir a Configuracion");
+    this.router.navigate(['/configuracion']);
   }
 }
 
