75c8937bd97471b5cfce96bb4be45d8178a1196f
Lorena Gonzalez <gonzalezm.lorena2@gmail.com>
2025-01-05 05:06:49 -0300

    Formulario y lista OK

--- src/app/componentes/cita-list/cita-list.component.ts
+++ src/app/componentes/cita-list/cita-list.component.ts
@@ -1,19 +1,23 @@
 import { CommonModule } from '@angular/common';   // Para usar el ngFor
 import { Component, Input, OnInit } from '@angular/core';
 import { Cita } from 'src/app/modelo/cita';
+import { IonItem, IonList, IonLabel, IonIcon, IonCol, IonGrid, IonRow  } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { trashOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-cita-list',
   templateUrl: './cita-list.component.html',
   styleUrls: ['./cita-list.component.scss'],
   standalone: true,
-  imports: [CommonModule]
+  imports: [CommonModule, IonItem, IonList, IonLabel, IonIcon, IonCol, IonGrid, IonRow  ]
 })
 
 export class CitaListComponent  implements OnInit {
 
   @Input() citas: Cita[] = []   // Recibo arreglo 
-  constructor() { }
+  constructor() {
+      addIcons({trashOutline}); }
 
   ngOnInit() {}
 
