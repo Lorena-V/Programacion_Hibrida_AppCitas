4f5a13c43003b8f737b658699cec80f850c9ed49
Lorena Gonzalez <gonzalezm.lorena2@gmail.com>
2025-01-05 02:57:55 -0300

    Navegación entre páginas usando router

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,40 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { IonButton, IonIcon, IonButtons,
           IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons'
-import { settingsOutline, buildOutline } from 'ionicons/icons'
-import { Router } from '@angular/router';
+import { buildOutline, settingsOutline } from 'ionicons/icons'
+// import { Router } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon, IonButtons
-   ],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon, 
+             IonButtons, RouterModule], 
 })
 
 export class HomePage {
 
-
-  constructor(private router:Router) {
+//   constructor(private router:Router) {
+//     addIcons({buildOutline});
+//       settingsOutline
+// }
+  constructor() {
       addIcons({buildOutline});
-
+        settingsOutline
   }
-
-  
 
   // Ruta hacia pag gestión
-  irGestionOnClick(){
-    console.log("Ir a gestion");
-    this.router.navigate(['/gestion']);
-  }
+  // irGestionOnClick(){
+  //   console.log("Ir a gestion");
+  //   this.router.navigate(['/gestion']);
+  // }
 
   // Ruta hacia pagina configuracion
-  irConfiguracionOnClick(){
-    console.log("Ir a Configuracion");
-    this.router.navigate(['/configuracion']);
-  }
+  // irConfiguracionOnClick(){
+  //   console.log("Ir a Configuracion");
+  //   this.router.navigate(['/configuracion']);
+  // }
 }
 
 
