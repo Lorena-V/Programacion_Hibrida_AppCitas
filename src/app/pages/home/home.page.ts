c3c0e4b724d5132ffb0ede9b065eaecdebca8e1a
Lorena Gonzalez <gonzalezm.lorena2@gmail.com>
2025-01-06 19:56:26 -0300

    Home listo

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonButton, IonIcon, IonButtons,
-          IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+          IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardTitle, IonCardHeader,
+          IonCardContent
+         } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons'
 import { buildOutline, settingsOutline } from 'ionicons/icons'
 // import { Router } from '@angular/router';
@@ -11,7 +13,8 @@ import { RouterModule } from '@angular/router';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon, 
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon,
+            IonCard, IonCardTitle, IonCardHeader, IonCardContent,
              IonButtons, RouterModule], 
 })
 
